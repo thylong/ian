3cd67b1addfc4aef8ee90af10a16cecac70a3aca
Theotime Leveque <theotime.leveque@dailymotion.com>
2017-02-22 15:51:44 -0500

    License: Mark all files as under license terms and conditions.

--- cmd/projects.go
+++ cmd/projects.go
@@ -1,4 +1,4 @@
-// Copyright © 2016 THEOTIME LEVEQUE <theotime.leveque@gmail.com>
+// Copyright © 2016 Theotime LEVEQUE theotime@protonmail.com
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
