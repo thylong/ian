04dc8f4ad22ea339b094e8dcc950bc1333fd19f1
thylong <theotime@protonmail.com>
2023-07-27 15:22:15 +0200

    feat(cli): drop show command

--- cmd/env.go
+++ cmd/env.go
@@ -1,16 +1,11 @@
 package cmd
 
 import (
-	"encoding/json"
-
 	"github.com/spf13/cobra"
-	"github.com/thylong/ian/pkg/config"
 	"github.com/thylong/ian/pkg/env"
 	"github.com/thylong/ian/pkg/log"
 	pm "github.com/thylong/ian/pkg/package-managers"
 )
-
-var allCmdParam bool
 
 func init() {
 	RootCmd.AddCommand(envCmd)
@@ -18,7 +13,6 @@ func init() {
 	RootCmd.AddCommand(
 		envAddCmd,
 		envRemoveCmd,
-		envShowCmd,
 		envSaveCmd,
 		envFreezeCmd,
 	)
@@ -76,17 +70,6 @@ var envRemoveCmd = &cobra.Command{
 
 		env.RemovePackagesFromEnvFile(packageManagerName, packages)
 		log.Infof("Package(s) removed to %s list\n", args[0])
-	},
-}
-
-var envShowCmd = &cobra.Command{
-	Use:   "show",
-	Short: "List all packages persisted in Ian configuration",
-	Long:  `List all packages persisted in Ian configuration.`,
-	Run: func(cmd *cobra.Command, args []string) {
-		settings := config.Vipers["env"].AllSettings()
-		prettySettings, _ := json.MarshalIndent(settings, "", "  ")
-		log.Infof("Configuration:\n%s\n}", prettySettings)
 	},
 }
 
